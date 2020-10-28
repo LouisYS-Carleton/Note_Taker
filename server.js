da099bc1151086754986ff04f8d4f816f8669122
Louis Yacksmith <louisyscarleton@gmail.com>
2020-10-28 19:49:12 -0400

    added routes to notes and default index.html, set up data parsing for express

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-// set up dependencies
+// Dependencies
 const express = require("express");
 const path = require("path");
 const fs = require('fs');
 const util = require('util');
 
-// set up express / port number
+// Variables
+const readFileHTML = util.promisify(fs.readFile)
+const writeFileHTML = util.promisify(fs.writeFile)
+
+// Set up express
 const app = express();
 const PORT = 3000;
 
-// set up data parse (express)
+// Set up data parse (express)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(_dirname, "./public")));
+
+
+
+
